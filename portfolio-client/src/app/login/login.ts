e39352daa405d13eed69616665bc2e2fbb10fe7f
Ervum <eervuml@gmail.com>
2025-12-15 18:22:23 -0300

    feat(): initial setup login component buttons.

--- portfolio-client/src/app/login/login.ts
+++ portfolio-client/src/app/login/login.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { ButtonComponent } from '../shared/components/standalone/button/button';
 import { TextboxComponent } from '../shared/components/standalone/textbox/textbox';
 
+import { AuthenticationService } from '../core/services/authentication/authentication';
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -11,7 +13,31 @@ import { TextboxComponent } from '../shared/components/standalone/textbox/textbo
   styleUrl: './login.scss'
 })
 export class LoginComponent {
+  private AuthenticationService: AuthenticationService = inject(AuthenticationService);
+  private Router: Router = inject(Router);
+
+  NavigateToHome(): void {
+    console.log('Navigating to home page from the login component!');
+    
+    this.Router.navigate(['/']);
+  }
+  
   HandleLogin(): void {
     console.log('Login attempt initiated from the login component!');
+
+    const UserPayload = {
+      Username: 'TestUser',
+      Password: 'TestPassword'
+    };
+
+    this.AuthenticationService.Register(UserPayload).subscribe({
+      next: (Response) => {
+        console.log('Successfully saved to Postgres:', Response);
+      },
+
+      error: (Error) => {
+        console.error('Error saving to Postgres:', Error);
+      }
+    });
   }
 }
